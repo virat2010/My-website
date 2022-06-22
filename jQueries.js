9a19d42180089e20df6ff91d74f9df054c8d99d0
virat2010 <59180983+virat2010@users.noreply.github.com>
2022-06-22 18:26:24 -0400

    asdas

--- jQueries.js
+++ jQueries.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
     $("hvr1").hover(function() {
+        $(this).hide();
+        $(this).fadeIn();
         $(this).text("You hovered over me!");
-        $(".hvr").animate({opacity:0.5});
     },function() {
-        $(this).text("Paris is the capital of France. Hover over me!");
-        $(".hvr").animate({opacity:1});
-    });
-    $("fcp").hover(function() {
-        $(this).text("This will take you to a fun project I made in Khan Academy.");
-    },function() {
-        $(this).text("Fun Canvas Project!");
+        $(this).hide();
+        $(this).fadeIn();
+        $(this).text("Paris is the capital of France. Hover over me!"); 
     });
 });
