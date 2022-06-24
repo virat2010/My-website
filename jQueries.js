56e600cca5073957d2b3ef382bd7d4c42e4acfad
virat2010 <59180983+virat2010@users.noreply.github.com>
2022-06-23 22:22:52 -0400

    asdsad

--- jQueries.js
+++ jQueries.js
@@ -1,11 +1,15 @@
+let x = "\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0"
+$("#rest").hide();
 $(document).ready(function() {
     $("hvr1").hover(function() {
         $(this).hide();
         $(this).fadeIn();
-        $(this).text("You hovered over me!");
+        $(this).text("You hovered over me!" +x+x+x);
+        $("#rest").fadeIn();
     },function() {
         $(this).hide();
         $(this).fadeIn();
-        $(this).text("Paris is the capital of France. Hover over me!"); 
+        $(this).text("Paris is the capital of France. Hover over me!");
+        $("#rest").fadeOut();
     });
 });
